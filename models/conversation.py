e8c44605532399d9fd236941ef516344b846c856
Omkar Kulkarni <2019okulkarn@tjhsst.edu>
2016-11-12 10:35:15 -0500

    add loggers only once
    Signed-off-by: Omkar Kulkarni <2019okulkarn@tjhsst.edu>

--- models/conversation.py
+++ models/conversation.py
@@ -17,12 +17,17 @@ class Conversation(BaseModel):
 
     @property
     def logger(self):
+        message_logger = logging.getLogger(self.id)
         message_formatter = logging.Formatter('[%(message_time)s] <%(username)s> %(message)s')
         file_handler = logging.FileHandler(os.path.join(settings.LOGGING_DIRECTORY, '{}.log'.format(self.id)))
         file_handler.setFormatter(message_formatter)
-        message_logger = logging.getLogger(self.id)
         message_logger.setLevel(logging.INFO)
-        message_logger.addHandler(file_handler)
+        if len(message_logger.handlers) > 0:
+            for handler in message_logger.handlers:
+                if not isinstance(handler, logging.FileHandler):
+                    message_logger.addHandler(file_handler)
+        else:
+            message_logger.addHandler(file_handler)
         return message_logger
 
     def __str__(self):
