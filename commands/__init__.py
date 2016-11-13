5521f39060f726601dde380d329452ace2dfb7d8
Omkar Kulkarni <2019okulkarn@tjhsst.edu>
2016-11-12 20:14:47 -0500

    dynamically import commands

--- commands/__init__.py
+++ commands/__init__.py
@@ -1,2 +1,15 @@
-from . import ping
-all_commands = [ping.command]
+import glob
+import importlib
+import settings
+import os
+import sys
+
+modules = glob.glob(os.path.join(settings.BASE_DIR, "commands") + "/*.py")
+
+all_commands = []
+for module in modules:
+    module_name = os.path.basename(module)[:-3]
+    if module_name == "__init__":
+        continue
+    importlib.import_module("commands.{}".format(module_name))
+    all_commands.append(sys.modules["commands.{}".format(module_name)].command)
